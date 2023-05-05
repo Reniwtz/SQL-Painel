4a076dc6a734c6cfee7c1fbc45c5cc9a837a26cc
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-05-05 15:27:32 -0300

    Update Atendimento_Quimioterapia.sql

--- Atendimento_Quimioterapia.sql
+++ Atendimento_Quimioterapia.sql
@@ -1,19 +1,18 @@
--- Quimioterapia SUS
+--Quimioterapia SUS
 SELECT 
     CASE 
         WHEN eve_siasus.cd_convenio IN ('1', '2') THEN 'SUS'
         WHEN eve_siasus.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
     END AS Convenio,
-    SUBSTR(TO_CHAR(eve_siasus.dt_eve_siasus, 'MONTH'),0,3) AS Mes_Atend,
-    COUNT(qt_lancada) AS CONT_CONV
+    TO_CHAR(eve_siasus.dt_eve_siasus, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE') AS Mes_Atend,
+    COUNT(qt_lancada) AS Quimioterapia
 FROM 
     eve_siasus eve_siasus,
-    convenio convenio,
-    empresa_convenio
+    convenio convenio
+    INNER JOIN empresa_convenio on empresa_convenio.cd_convenio = convenio.cd_convenio
 WHERE 
-    empresa_convenio.cd_convenio = convenio.cd_convenio
-    AND convenio.cd_convenio = '1'
+        convenio.cd_convenio = '1'
     AND eve_siasus.dt_eve_siasus BETWEEN ( '01/01/2023' ) AND ( '31/03/2023' )
     AND eve_siasus.cd_apac IS NOT NULL
     AND eve_siasus.cd_procedimento LIKE ( '0304%' )
@@ -26,22 +25,22 @@ GROUP BY
         WHEN eve_siasus.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
     END,
-    SUBSTR(TO_CHAR(eve_siasus.dt_eve_siasus, 'MONTH'),0,3),
+    TO_CHAR(eve_siasus.dt_eve_siasus, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE'),
     convenio.nm_convenio   
 ORDER BY 
-    Mes_Atend;
+    TO_DATE(mes_atend, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE');
 
 
 
--- Quimioterapia Particular e Convênio
+--Quimioterapia Particular e Convênio
 SELECT
     CASE 
         WHEN atendime.cd_convenio IN ('1', '2') THEN 'SUS'
         WHEN atendime.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
     END AS Convenio,
-    SUBSTR(TO_CHAR(atendime.dt_atendimento, 'MONTH'),0,3) AS Mes_Atend,
-    COUNT(atendime.cd_convenio) AS CONT_CONV
+    TO_CHAR(atendime.dt_atendimento, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE') AS Mes_Atend,
+    COUNT(atendime.cd_convenio) AS Quimioterapia
 FROM
     atendime atendime,
     convenio convenio
@@ -56,8 +55,8 @@ GROUP BY
         WHEN atendime.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
     END,
-    SUBSTR(TO_CHAR(atendime.dt_atendimento, 'MONTH'),0,3),
+    TO_CHAR(atendime.dt_atendimento, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE'),
     convenio.nm_convenio, 
     atendime.cd_procedimento
 ORDER BY 
-    Mes_Atend;
+    TO_DATE(mes_atend, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE');
