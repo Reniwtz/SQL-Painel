59cac35b7b56a3c916704e1d21b36c03afbc3d26
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-05-05 15:10:31 -0300

    Update Total_de_Cirurgias.sql

--- Total_de_Cirurgias.sql
+++ Total_de_Cirurgias.sql
@@ -1,30 +1,28 @@
--- Cirurgia SUS, Particular e Convênio
-SELECT 
-    CASE 
-        WHEN cirurgia_aviso.cd_convenio IN ('1', '2') THEN 'SUS'
-        WHEN cirurgia_aviso.cd_convenio = '16' THEN 'Particular'
+-- Total de Atendimentos de Urgência SUS, Particular e Convênios
+SELECT
+    CASE
+        WHEN atendime.cd_convenio IN ( '1', '2' ) THEN 'SUS'
+        WHEN atendime.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
     END AS Convenio,
-    TO_CHAR(aviso_cirurgia.dt_realizacao, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE') AS Mes_Atend,
-    COUNT(cirurgia_aviso.cd_convenio) AS Cirúrgia
-FROM 
-    aviso_cirurgia aviso_cirurgia
-    INNER JOIN cirurgia_aviso ON aviso_cirurgia.cd_aviso_cirurgia = cirurgia_aviso.cd_aviso_cirurgia
-    INNER JOIN cirurgia ON cirurgia_aviso.cd_cirurgia = cirurgia.cd_cirurgia,
+    TO_CHAR(atendime.dt_atendimento, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE') AS mes_atend,
+    COUNT(atendime.cd_convenio) AS Urgência
+FROM
+    atendime atendime,
     convenio convenio
     INNER JOIN empresa_convenio on empresa_convenio.cd_convenio = convenio.cd_convenio
-WHERE 
-        aviso_cirurgia.tp_situacao = 'R'
+WHERE
+        trunc(atendime.dt_atendimento) BETWEEN '01/04/2023' AND '30/04/2023'
+    AND dbamv.fnc_mv_usuario_unidade_setor(NULL, NULL, atendime.cd_ori_ate, atendime.cd_leito) = 'S'
+    AND atendime.tp_atendimento = 'U'
     AND convenio.cd_convenio = '1'
-    AND aviso_cirurgia.cd_cen_cir = '1'
-    AND aviso_cirurgia.dt_realizacao BETWEEN ('01/01/2023') AND ('31/03/2023')
-GROUP BY 
+GROUP BY
     CASE 
-        WHEN cirurgia_aviso.cd_convenio IN ('1', '2') THEN 'SUS'
-        WHEN cirurgia_aviso.cd_convenio = '16' THEN 'Particular'
+        WHEN atendime.cd_convenio IN ('1', '2') THEN 'SUS'
+        WHEN atendime.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
     END,
-    TO_CHAR(aviso_cirurgia.dt_realizacao, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE'),
-    convenio.nm_convenio   
-ORDER BY 
-    TO_DATE(mes_atend, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE');
+    TO_CHAR(atendime.dt_atendimento, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE'),
+    convenio.nm_convenio
+ORDER BY
+     TO_DATE(mes_atend, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE');
