e5b28a82703e7539d490d771c45bd87e8f7c5e7e
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-05-05 14:39:16 -0300

    Update Total_de_Cirurgias.sql

--- Total_de_Cirurgias.sql
+++ Total_de_Cirurgias.sql
@@ -5,19 +5,16 @@ SELECT
         WHEN cirurgia_aviso.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
     END AS Convenio,
-    SUBSTR(TO_CHAR(aviso_cirurgia.dt_realizacao, 'MONTH'),0,3) AS Mes_Atend,
-    COUNT(cirurgia_aviso.cd_convenio) AS CONT_CONV
+    TO_CHAR(aviso_cirurgia.dt_realizacao, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE') AS Mes_Atend,
+    COUNT(cirurgia_aviso.cd_convenio) AS Cirúrgia
 FROM 
-    aviso_cirurgia aviso_cirurgia,
-    cirurgia_aviso cirurgia_aviso,
-    cirurgia cirurgia,
-    convenio convenio,
-    empresa_convenio
+    aviso_cirurgia aviso_cirurgia
+    INNER JOIN cirurgia_aviso ON aviso_cirurgia.cd_aviso_cirurgia = cirurgia_aviso.cd_aviso_cirurgia
+    INNER JOIN cirurgia ON cirurgia_aviso.cd_cirurgia = cirurgia.cd_cirurgia,
+    convenio convenio
+    INNER JOIN empresa_convenio on empresa_convenio.cd_convenio = convenio.cd_convenio
 WHERE 
-    empresa_convenio.cd_convenio = convenio.cd_convenio
-    AND aviso_cirurgia.tp_situacao = 'R'
-    AND aviso_cirurgia.cd_aviso_cirurgia = cirurgia_aviso.cd_aviso_cirurgia
-    AND cirurgia_aviso.cd_cirurgia = cirurgia.cd_cirurgia
+        aviso_cirurgia.tp_situacao = 'R'
     AND convenio.cd_convenio = '1'
     AND aviso_cirurgia.cd_cen_cir = '1'
     AND aviso_cirurgia.dt_realizacao BETWEEN ('01/01/2023') AND ('31/03/2023')
@@ -27,7 +24,7 @@ GROUP BY
         WHEN cirurgia_aviso.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
     END,
-    SUBSTR(TO_CHAR(aviso_cirurgia.dt_realizacao, 'MONTH'),0,3),
+    TO_CHAR(aviso_cirurgia.dt_realizacao, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE'),
     convenio.nm_convenio   
 ORDER BY 
-    Mes_Atend;
+    TO_DATE(mes_atend, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE');
