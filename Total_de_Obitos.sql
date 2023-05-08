657b7e47b7e4a6c69ffb776813d3ca79e9422487
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-05-08 10:17:02 -0300

    Update Total_de_Obitos.sql

--- Total_de_Obitos.sql
+++ Total_de_Obitos.sql
@@ -1,27 +1,29 @@
--- Total de internações SUS, Particular e Convênios
+-- Total de Obitos SUS, Particular e Convênios
 SELECT
-    CASE 
-        WHEN atendime.cd_convenio IN ('1', '2') THEN 'SUS'
+    CASE
+        WHEN atendime.cd_convenio IN ( '1', '2' ) THEN 'SUS'
         WHEN atendime.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
-    END AS Convenio,
-    TO_CHAR(atendime.dt_atendimento, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE') AS Mes_Atend,
-    COUNT(atendime.cd_convenio) AS Internação
+    END AS convenio,
+    to_char(atendime.dt_alta, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE') AS mes_atend,
+    COUNT(atendime.cd_convenio) AS cont_conv
 FROM
-    atendime atendime,
+    atendime atendime
+    INNER JOIN paciente on atendime.cd_paciente = paciente.cd_paciente, 
     convenio convenio
     INNER JOIN empresa_convenio on empresa_convenio.cd_convenio = convenio.cd_convenio
 WHERE
-        convenio.cd_convenio = '1'
-    AND atendime.tp_atendimento = 'I'
-    AND atendime.dt_atendimento BETWEEN ( '01/01/2023' ) AND ( '30/04/2023' )
-GROUP BY 
-    CASE 
-        WHEN atendime.cd_convenio IN ('1', '2') THEN 'SUS'
+    atendime.sn_obito = 'S'
+    AND atendime.dt_alta BETWEEN ('01/01/2023') AND ('30/03/2023')
+    AND atendime.cd_mot_alt IS NOT NULL
+    AND convenio.cd_convenio = '1'
+GROUP BY
+    CASE
+        WHEN atendime.cd_convenio IN ( '1', '2' ) THEN 'SUS'
         WHEN atendime.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
     END,
-    TO_CHAR(atendime.dt_atendimento, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE'),
-    convenio.nm_convenio   
-ORDER BY 
-    TO_DATE(mes_atend, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE');
+    to_char(atendime.dt_alta, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE'),
+    convenio.nm_convenio
+ORDER BY
+    TO_DATE(mes_atend, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE')
