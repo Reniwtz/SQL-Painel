047620f0c9ffd96ebebef69e9bddbf15ef295f66
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-05-09 17:56:51 -0300

    Update Total_de_Obitos.sql

--- Total_de_Obitos.sql
+++ Total_de_Obitos.sql
@@ -6,24 +6,21 @@ SELECT
         ELSE 'P.Saude'
     END AS convenio,
     to_char(atendime.dt_alta, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE') AS mes_atend,
-    COUNT(atendime.cd_convenio) AS cont_conv
+    COUNT(CASE WHEN atendime.sn_obito = 'S' THEN 1 ELSE NULL END) AS cont_conv
 FROM
     atendime atendime
-    INNER JOIN paciente on atendime.cd_paciente = paciente.cd_paciente, 
-    convenio convenio
-    INNER JOIN empresa_convenio on empresa_convenio.cd_convenio = convenio.cd_convenio
+    LEFT JOIN paciente paciente ON atendime.cd_paciente = paciente.cd_paciente
+    LEFT JOIN convenio convenio ON atendime.cd_convenio = convenio.cd_convenio
+    LEFT JOIN empresa_convenio ON empresa_convenio.cd_convenio = convenio.cd_convenio
 WHERE
-    atendime.sn_obito = 'S'
-    AND atendime.dt_alta BETWEEN ('01/01/2023') AND ('30/03/2023')
+      atendime.dt_alta BETWEEN ('01/01/2022') AND ('30/12/2022')
     AND atendime.cd_mot_alt IS NOT NULL
-    AND convenio.cd_convenio = '1'
 GROUP BY
     CASE
         WHEN atendime.cd_convenio IN ( '1', '2' ) THEN 'SUS'
         WHEN atendime.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
     END,
-    to_char(atendime.dt_alta, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE'),
-    convenio.nm_convenio
+    to_char(atendime.dt_alta, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE')
 ORDER BY
-    TO_DATE(mes_atend, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE')
+    TO_DATE(mes_atend, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE')   
