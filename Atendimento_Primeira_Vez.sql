20e18a6e14d9f7ca018353a44efca05666995387
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-05-12 08:43:40 -0300

    Update Atendimento_Primeira_Vez.sql

--- Atendimento_Primeira_Vez.sql
+++ Atendimento_Primeira_Vez.sql
@@ -1,4 +1,4 @@
--- Atendimento de primeira vez por Municipio especifico
+-- Atendimento de Primeira vez por Municipio Especifico
 SELECT
     c.nm_cidade AS Cidade,
     c.cd_uf AS Estado,
@@ -38,7 +38,58 @@ ORDER BY
     COUNT(*);
    
    
--- Total de atendimento de primeira vez   
+-- Total de atendimento de Primeira Vez   
 SELECT COUNT(p.cd_paciente)
   FROM paciente p
- WHERE p.dt_cadastro BETWEEN ( '01/01/2021' ) AND ( '31/12/2021' );    
+ WHERE p.dt_cadastro BETWEEN ( '01/01/2021' ) AND ( '31/12/2021' );   
+ 
+ 
+-----------------------------------------------------------------------------
+    
+    
+-- Outros atendimentos por Municípios
+SELECT
+    c.nm_cidade AS cidade,
+    COUNT(*)    AS total_de_atendimento
+FROM
+    atendime a,
+    paciente q,
+    cidade   c
+WHERE
+        a.cd_paciente = q.cd_paciente
+    AND c.cd_cidade = q.cd_cidade
+    AND a.dt_atendimento BETWEEN ( '01/01/2022' ) AND ( '31/12/2022' )
+    AND ( c.nm_cidade = 'JOAO PESSOA'
+          OR c.nm_cidade = 'CABEDELO'
+          OR c.nm_cidade = 'SANTA RITA'
+          OR c.nm_cidade = 'BAYEUX'
+          OR c.nm_cidade = 'GUARABIRA'
+          OR c.nm_cidade = 'PATOS'
+          OR c.nm_cidade = 'MAMANGUAPE'
+          OR c.nm_cidade = 'SAPE'
+          OR c.nm_cidade = 'ITABAIANA'
+          OR c.nm_cidade = 'JURIPIRANGA'
+          OR c.nm_cidade = 'CONDE'
+          OR c.nm_cidade = 'CAMPINA GRANDE'
+          OR c.nm_cidade = 'RIO TINTO'
+          OR c.nm_cidade = 'ALHANDRA'
+          OR c.nm_cidade = 'LUCENA'
+          OR c.nm_cidade = 'LUCENA'
+          OR c.nm_cidade = 'PEDRAS DE FOGO'
+          OR c.nm_cidade = 'PILAR'
+          OR c.nm_cidade = 'CAAPORA'
+          OR c.nm_cidade = 'SOUSA'
+          OR c.nm_cidade = 'SALGADO DE SAO FELIX' )
+GROUP BY
+    c.nm_cidade
+ORDER BY
+    COUNT(*);
+
+
+ -- Total geral de outros atendimentos
+SELECT
+    COUNT(a.cd_atendimento)
+FROM
+    atendime a
+WHERE
+    a.dt_atendimento BETWEEN ( '01/01/2022' ) AND ( '31/12/2022' );
