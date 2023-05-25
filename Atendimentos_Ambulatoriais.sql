6fbadea6390bb532cb9c83a1b34f08125ccc77a6
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-05-25 10:51:55 -0300

    Update Atendimentos_Ambulatoriais.sql

--- Atendimentos_Ambulatoriais.sql
+++ Atendimentos_Ambulatoriais.sql
@@ -1,27 +1,25 @@
--- Atendimentos SUS, Particular e Convênios
-SELECT 
-    CASE 
-        WHEN atendime.cd_convenio IN ('1', '2') THEN 'SUS'
-        WHEN atendime.cd_convenio = '16' THEN 'Particular'
+SELECT
+    CASE
+        WHEN ped_rx.cd_convenio IN ( '1', '2' ) THEN 'SUS'
+        WHEN ped_rx.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
-    END AS Convenio,
-    TO_CHAR(atendime.dt_atendimento, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE') AS Mes_Atend,
-    COUNT(atendime.cd_convenio) AS Ambulatorias
-FROM 
-    atendime atendime,
-    convenio convenio
-    INNER JOIN empresa_convenio on empresa_convenio.cd_convenio = convenio.cd_convenio
-WHERE 
-        convenio.cd_convenio = '1'
-    AND atendime.tp_atendimento = 'A'
-    AND atendime.dt_atendimento BETWEEN ( '01/01/2023' ) AND ( '31/03/2023' )
-GROUP BY 
-    CASE 
-        WHEN atendime.cd_convenio IN ('1', '2') THEN 'SUS'
-        WHEN atendime.cd_convenio = '16' THEN 'Particular'
-        ELSE 'P.Saude'
-    END,
-    TO_CHAR(atendime.dt_atendimento, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE'),
-    convenio.nm_convenio   
-ORDER BY 
+    END AS convenio,
+    to_char(itped_rx.dt_realizado, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE') AS mes_atend,
+    COUNT(ped_rx.cd_convenio) AS ambulatorias
+FROM
+    dbamv.itped_rx
+    INNER JOIN exa_rx ON itped_rx.cd_exa_rx = exa_rx.cd_exa_rx
+    INNER JOIN ped_rx ON itped_rx.cd_ped_rx = ped_rx.cd_ped_rx
+    INNER JOIN convenio ON ped_rx.cd_convenio = convenio.cd_convenio
+WHERE
+        itped_rx.dt_realizado BETWEEN '01/04/2023' AND '30/04/2023'
+    AND ped_rx.cd_set_exa = 13
+GROUP BY
+        CASE
+            WHEN ped_rx.cd_convenio IN ( '1', '2' ) THEN 'SUS'
+            WHEN ped_rx.cd_convenio = '16' THEN 'Particular'
+            ELSE 'P.Saude'
+        END,
+        to_char(itped_rx.dt_realizado, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE')
+ORDER BY
     TO_DATE(mes_atend, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE');
