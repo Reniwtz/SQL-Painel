65a8262513d478875be700ef09177911dec8c1fb
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-08-16 10:32:33 -0300

    Update Atendimento_de_Fonoaudiologia.sql

--- Atendimento_de_Fonoaudiologia.sql
+++ Atendimento_de_Fonoaudiologia.sql
@@ -4,22 +4,21 @@ SELECT
         WHEN atendime.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
     END AS Convenio,
-    TO_CHAR(atendime.dt_atendimento, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE') AS Mes_Atend,
-    COUNT(atendime.cd_convenio) AS FONOAUDIOLOGIA
+    TO_CHAR(atendime.dt_atendimento, 'MON','NLS_DATE_LANGUAGE=PORTUGUESE') AS Mes_Atend,
+    COUNT(CASE WHEN atendime.cd_especialid = '54' THEN 1 ELSE NULL END) AS FONOAUDIOLOGIA
 FROM
     atendime atendime,
     convenio convenio
 WHERE
     convenio.cd_convenio = '1'
-    AND cd_especialid LIKE '54'
-    AND atendime.dt_atendimento BETWEEN ( '01/04/2023' ) AND ( '30/04/2023' ) 
+    AND EXTRACT(YEAR FROM dt_atendimento)  = '2023' 
 GROUP BY 
     CASE 
         WHEN atendime.cd_convenio IN ('1', '2') THEN 'SUS'
         WHEN atendime.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
     END,
-    TO_CHAR(atendime.dt_atendimento, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE'),
+    TO_CHAR(atendime.dt_atendimento, 'MON','NLS_DATE_LANGUAGE=PORTUGUESE'),
     convenio.nm_convenio   
 ORDER BY 
     TO_DATE(mes_atend, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE');
