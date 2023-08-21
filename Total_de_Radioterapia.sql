c0057495fe51b05c88b3088e02c201d23f5cf455
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2023-08-21 17:45:49 -0300

    Update Total_de_Radioterapia.sql

--- Total_de_Radioterapia.sql
+++ Total_de_Radioterapia.sql
@@ -61,3 +61,39 @@ GROUP BY
     atendime.cd_procedimento
 ORDER BY 
     TO_DATE(mes_atend, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE');
+
+
+
+-- Consultas de Radioterapia SUS
+SELECT
+    CASE
+        WHEN atendime.cd_convenio IN ( '1', '2' ) THEN 'SUS'
+        WHEN atendime.cd_convenio = '16' THEN 'Particular'
+        ELSE 'P.Saude'
+    END AS convenio,
+    to_char(atendime.dt_atendimento, 'MON', 'NLS_DATE_LANGUAGE=PORTUGUESE') AS mes_atend,
+    COUNT(CASE WHEN atendime.cd_ori_ate like 14 THEN 1 ELSE NULL END) AS cont_conv
+FROM
+    atendime atendime,
+    convenio convenio
+    INNER JOIN empresa_convenio ON empresa_convenio.cd_convenio = convenio.cd_convenio
+WHERE
+      convenio.cd_convenio = '1'
+    AND atendime.dt_atendimento BETWEEN TO_DATE('01/06/2023', 'DD/MM/YYYY') AND TO_DATE('30/06/2023', 'DD/MM/YYYY')
+    --AND atendime.cd_ori_ate like 14
+    AND (cd_tip_mar like 2
+         OR cd_tip_mar LIKE 4
+         OR cd_tip_mar LIKE 7)
+    AND (atendime.cd_prestador like '216'
+        OR atendime.cd_prestador like '2046'
+        OR atendime.cd_prestador like '3893'
+        OR atendime.cd_prestador like '83')
+GROUP BY
+    CASE
+        WHEN atendime.cd_convenio IN ( '1', '2' ) THEN 'SUS'
+        WHEN atendime.cd_convenio = '16' THEN 'Particular'
+        ELSE 'P.Saude'
+    END,
+    to_char(atendime.dt_atendimento, 'MON', 'NLS_DATE_LANGUAGE=PORTUGUESE')
+ORDER BY
+    TO_DATE(mes_atend, 'MON', 'NLS_DATE_LANGUAGE=PORTUGUESE')   
