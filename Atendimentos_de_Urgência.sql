480b81f05b2ed0b04142212fa1883fa53826efcb
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-10-15 10:50:44 -0300

    Update Atendimentos_de_Urgência.sql

--- Atendimentos_de_Urgência.sql
+++ Atendimentos_de_Urgência.sql
@@ -1,27 +1,72 @@
--- Total de Atendimentos de Urgência SUS, Particular e Convênios
+-- MÊs
 SELECT
     CASE
-        WHEN atendime.cd_convenio IN ( '1', '2' ) THEN 'SUS'
-        WHEN atendime.cd_convenio = '16' THEN 'Particular'
-        ELSE 'P.Saude'
-    END AS Convenio,
-    TO_CHAR(atendime.dt_atendimento, 'MON','NLS_DATE_LANGUAGE=PORTUGUESE') AS mes_atend,
-    COUNT(CASE WHEN atendime.tp_atendimento = 'U' THEN 1 ELSE NULL END) AS Urgência
+        WHEN atendime.cd_convenio IN ( '1', '2' ) THEN
+            'SUS'
+        WHEN atendime.cd_convenio = '16' THEN
+            'Particular'
+        ELSE
+            'P.Saude'
+    END                                                                     AS convenio,
+    to_char(atendime.dt_atendimento, 'MON', 'NLS_DATE_LANGUAGE=PORTUGUESE') AS mes_atend,
+    COUNT(
+        CASE
+            WHEN atendime.tp_atendimento = 'U' THEN
+                1
+            ELSE
+                NULL
+        END
+    )                                                                       AS urgência
 FROM
-    atendime atendime,
-    convenio convenio
-    INNER JOIN empresa_convenio on empresa_convenio.cd_convenio = convenio.cd_convenio
+    atendime atendime
 WHERE
-        EXTRACT(YEAR FROM atendime.dt_atendimento) = '2023'
-    AND dbamv.fnc_mv_usuario_unidade_setor(NULL, NULL, atendime.cd_ori_ate, atendime.cd_leito) = 'S'
-    AND convenio.cd_convenio = '1'
+        atendime.dt_atendimento BETWEEN TO_DATE('01/01/23', 'DD/MM/YY') AND TO_DATE('31/12/23', 'DD/MM/YY')
 GROUP BY
-    CASE 
-        WHEN atendime.cd_convenio IN ('1', '2') THEN 'SUS'
-        WHEN atendime.cd_convenio = '16' THEN 'Particular'
-        ELSE 'P.Saude'
-    END,
-    TO_CHAR(atendime.dt_atendimento, 'MON','NLS_DATE_LANGUAGE=PORTUGUESE'),
-    convenio.nm_convenio
+        CASE
+            WHEN atendime.cd_convenio IN ( '1', '2' ) THEN
+                'SUS'
+            WHEN atendime.cd_convenio = '16' THEN
+                'Particular'
+            ELSE
+                'P.Saude'
+        END,
+        to_char(atendime.dt_atendimento, 'MON', 'NLS_DATE_LANGUAGE=PORTUGUESE')
 ORDER BY
-     TO_DATE(mes_atend, 'MON', 'NLS_DATE_LANGUAGE=PORTUGUESE');
+    TO_DATE(mes_atend, 'MON', 'NLS_DATE_LANGUAGE=PORTUGUESE') 
+
+
+-- ANO
+SELECT
+    CASE
+        WHEN atendime.cd_convenio IN ( '1', '2' ) THEN
+            'SUS'
+        WHEN atendime.cd_convenio = '16' THEN
+            'Particular'
+        ELSE
+            'P.Saude'
+    END                                                                      AS convenio,
+    to_char(atendime.dt_atendimento, 'YYYY', 'NLS_DATE_LANGUAGE=PORTUGUESE') AS mes_atend,
+    COUNT(
+        CASE
+            WHEN atendime.tp_atendimento = 'U' THEN
+                1
+            ELSE
+                NULL
+        END
+    )                                                                        AS urgência
+FROM
+    atendime atendime
+WHERE
+    EXTRACT(YEAR FROM atendime.dt_atendimento) BETWEEN '2006' AND '2024'
+GROUP BY
+        CASE
+            WHEN atendime.cd_convenio IN ( '1', '2' ) THEN
+                'SUS'
+            WHEN atendime.cd_convenio = '16' THEN
+                'Particular'
+            ELSE
+                'P.Saude'
+        END,
+        to_char(atendime.dt_atendimento, 'YYYY', 'NLS_DATE_LANGUAGE=PORTUGUESE')
+ORDER BY
+    to_date(mes_atend, 'YYYY', 'NLS_DATE_LANGUAGE=PORTUGUESE')
