ac1cf70a4c56a52fe3f25d8a5b1bdb9203829d44
Renilton Ribeiro <41738482+Reniwtz@users.noreply.github.com>
2024-10-15 08:16:06 -0300

    Update Total_de_Cirurgias.sql

--- Total_de_Cirurgias.sql
+++ Total_de_Cirurgias.sql
@@ -1,30 +1,67 @@
--- Cirurgia SUS, Particular e Convênio
-SELECT 
-    CASE 
-        WHEN cirurgia_aviso.cd_convenio IN ('1', '2') THEN 'SUS'
+-- Cirurgia Mês
+SELECT
+    CASE
+        WHEN cirurgia_aviso.cd_convenio IN ( '1', '2' ) THEN
+            'SUS'
+        WHEN cirurgia_aviso.cd_convenio = '16' THEN
+            'Particular'
+        ELSE
+            'P.Saude'
+    END                               AS convenio,
+    substr(to_char(aviso_cirurgia.dt_realizacao, 'MON', 'NLS_DATE_LANGUAGE = PORTUGUESE'),
+           0,
+           3)                         mes_atend,
+    COUNT(cirurgia_aviso.cd_convenio) AS cont_conv,
+    TO_NUMBER(TO_CHAR(aviso_cirurgia.dt_realizacao, 'MM')) AS mes_numero  -- Adiciona o número do mês
+FROM
+         aviso_cirurgia aviso_cirurgia
+    INNER JOIN cirurgia_aviso cirurgia_aviso ON aviso_cirurgia.cd_aviso_cirurgia = cirurgia_aviso.cd_aviso_cirurgia
+    INNER JOIN cirurgia ON cirurgia_aviso.cd_cirurgia = cirurgia.cd_cirurgia
+WHERE
+        aviso_cirurgia.tp_situacao = 'R'
+    AND aviso_cirurgia.cd_cen_cir = '1'
+    AND EXTRACT(YEAR FROM aviso_cirurgia.dt_realizacao) = '2024'
+GROUP BY
+        CASE
+            WHEN cirurgia_aviso.cd_convenio IN ( '1', '2' ) THEN
+                'SUS'
+            WHEN cirurgia_aviso.cd_convenio = '16' THEN
+                'Particular'
+            ELSE
+                'P.Saude'
+        END,
+        substr(to_char(aviso_cirurgia.dt_realizacao, 'MON', 'NLS_DATE_LANGUAGE = PORTUGUESE'),
+               0,
+               3),
+        TO_NUMBER(TO_CHAR(aviso_cirurgia.dt_realizacao, 'MM'))  -- Agrupa pelo número do mês
+ORDER BY
+    mes_numero  -- Ordena pelo número do mês para garantir a ordem cronológica
+
+
+
+-- Cirurgia Ano
+SELECT
+    CASE
+        WHEN cirurgia_aviso.cd_convenio IN ( '1', '2' ) THEN 'SUS'
         WHEN cirurgia_aviso.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
-    END AS Convenio,
-    TO_CHAR(aviso_cirurgia.dt_realizacao, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE') AS Mes_Atend,
-    COUNT(cirurgia_aviso.cd_convenio) AS Cirurgia
-FROM 
+    END AS convenio,
+    to_char(aviso_cirurgia.dt_realizacao, 'YYYY', 'NLS_DATE_LANGUAGE=PORTUGUESE') AS ano_atend,
+    COUNT(cirurgia_aviso.cd_convenio) AS cirurgia
+FROM
     aviso_cirurgia aviso_cirurgia
     INNER JOIN cirurgia_aviso ON aviso_cirurgia.cd_aviso_cirurgia = cirurgia_aviso.cd_aviso_cirurgia
-    INNER JOIN cirurgia ON cirurgia_aviso.cd_cirurgia = cirurgia.cd_cirurgia,
-    convenio convenio
-    INNER JOIN empresa_convenio on empresa_convenio.cd_convenio = convenio.cd_convenio
-WHERE 
-        aviso_cirurgia.tp_situacao = 'R'
-    AND convenio.cd_convenio = '1'
+    INNER JOIN cirurgia ON cirurgia_aviso.cd_cirurgia = cirurgia.cd_cirurgia
+WHERE
+    aviso_cirurgia.tp_situacao = 'R'
     AND aviso_cirurgia.cd_cen_cir = '1'
-    AND aviso_cirurgia.dt_realizacao BETWEEN ('01/01/2023') AND ('31/03/2023')
-GROUP BY 
-    CASE 
-        WHEN cirurgia_aviso.cd_convenio IN ('1', '2') THEN 'SUS'
+    AND EXTRACT(YEAR FROM aviso_cirurgia.dt_realizacao) BETWEEN 2006 AND 2024-- Substituir por parâmetro
+GROUP BY
+    CASE
+        WHEN cirurgia_aviso.cd_convenio IN ( '1', '2' ) THEN 'SUS'
         WHEN cirurgia_aviso.cd_convenio = '16' THEN 'Particular'
         ELSE 'P.Saude'
     END,
-    TO_CHAR(aviso_cirurgia.dt_realizacao, 'MONTH','NLS_DATE_LANGUAGE=PORTUGUESE'),
-    convenio.nm_convenio   
-ORDER BY 
-    TO_DATE(mes_atend, 'MONTH', 'NLS_DATE_LANGUAGE=PORTUGUESE');
+    to_char(aviso_cirurgia.dt_realizacao, 'YYYY', 'NLS_DATE_LANGUAGE=PORTUGUESE')
+ORDER BY
+    ano_atend; -- Apenas ordenar pelo ano diretamente
